916f05f5a777c2cffcbf7fe85eea1c2946b1bcca
Alexandre Cordeiro <accordeiro@gmail.com>
2014-09-19 12:32:52 -0300

    Code clean-up on scraper.js;

--- scraper.js
+++ scraper.js
@@ -1,6 +1,6 @@
-var fs = require('fs');
-var cheerio = require('cheerio');
-var request = require('request');
+var fs        = require('fs');
+var cheerio   = require('cheerio');
+var request   = require('request');
 
 var twitter_username = process.argv[2];
 if (twitter_username) {
@@ -8,14 +8,14 @@ if (twitter_username) {
   request(twitter_url, function(error, response, html) {
     if (!error) {
       var $ = cheerio.load(html);
+      var $wrapper = $('.ProfileHeaderCard');
       var name, screenname, bio, location, webpage, join_date;
 
-      var $wrapper = $('.ProfileHeaderCard');
-      name = $wrapper.find('.ProfileHeaderCard-name a').first().text();
-      screenname = $wrapper.find('.ProfileHeaderCard-screenname > a').first().text();
-      bio = $wrapper.find('.ProfileHeaderCard-bio').first().text();
-      webpage = $wrapper.find('.ProfileHeaderCard-url a').first().text();
-      join_date = $wrapper.find('.ProfileHeaderCard-joinDate').first().text();
+      name        = $wrapper.find('.ProfileHeaderCard-name a').first().text();
+      screenname  = $wrapper.find('.ProfileHeaderCard-screenname > a').first().text();
+      bio         = $wrapper.find('.ProfileHeaderCard-bio').first().text();
+      webpage     = $wrapper.find('.ProfileHeaderCard-url a').first().text();
+      join_date   = $wrapper.find('.ProfileHeaderCard-joinDate').first().text();
 
       var response = {
         name: name,
@@ -24,7 +24,8 @@ if (twitter_username) {
         webpage: webpage,
         join_date: join_date
       };
-      console.log(response)
+
+      console.log(response);
     }
 
   });
